95322c46767ae4713430ab4648eca33ebeb21415
jesus <jv176118@gmail.com>
2026-07-23 17:25:48 -0300

    Update: Se actualizo el cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,11 +5,11 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => array_values(array_filter(array_map(
         'trim',
-        explode(',', env('FRONTEND_URLS', 'http://localhost:3000,http://127.0.0.1:3000')),
+        explode(',', env('FRONTEND_URLS', 'http://localhost:3000,http://127.0.0.1:3000,https://test.pagina.iesnuevohorizonte.com,https://test.pagina.iesnuevohorizonte.com/')),
     ))),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'Accept', 'Origin'],
-    'exposed_headers' => [],
+    'exposed_headers' => [], 
     'max_age' => 3600,
     'supports_credentials' => false,
 ];
